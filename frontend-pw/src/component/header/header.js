c16fcac9df2cf9c21508d29c96bd2e0fe796ef18
Damian Li <ke_2021@126.com>
2023-12-16 15:47:07 -0500

    mobile media header fixed

--- frontend-pw/src/component/header/header.js
+++ frontend-pw/src/component/header/header.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from "../navbar/navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCompass} from '@fortawesome/free-solid-svg-icons'
 import "./header.css";
 
 function Header () {
@@ -26,6 +28,13 @@ function Header () {
         window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scroll to top
     };
 
+    // for mobile screen header
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
   return (
     <div className={`App-header ${scrolled ? 'scrolled' : ''}`}>
     <section className="header">
@@ -35,6 +44,18 @@ function Header () {
         <section className="header-top__navbar">
             <Navbar /> 
         </section>
+        <section className="mobile-navbar" onClick={toggleMenu}>
+          {/* 菜单图标或者文字 */}
+          <FontAwesomeIcon icon={faCompass} />
+        </section>
+        {isMenuOpen && (
+          <div className={`mobile-menu ${isMenuOpen ? 'mobile-menu-open' : ''}`}>
+            {/* 移动菜单内容 */}
+            <a href="#about" className="navbar-item">About Me</a>
+            <a href="#projects" className="navbar-item">Projects</a>
+            <a href="/blog" className="navbar-item">Blog</a>
+          </div>
+        )}
 
     </section>
     </div>
