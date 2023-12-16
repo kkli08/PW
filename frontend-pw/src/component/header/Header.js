7f1928cac7a5d1fc6c6542eda643932370f1381c
Damian Li <ke_2021@126.com>
2023-12-15 23:40:04 -0500

    adjust header

--- frontend-pw/src/component/header/Header.js
+++ frontend-pw/src/component/header/Header.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import Navbar from "../navbar/navbar";
 import "./header.css";
 
 function Header () {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 0;
+            setScrolled(isScrolled);
+        };
+
+        // 添加滚动监听
+        window.addEventListener('scroll', handleScroll);
+
+        // 清除监听
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
   return (
-    
+    <div className={`App-header ${scrolled ? 'scrolled' : ''}`}>
     <section className="header">
         <section className="header-top__logo">
             <a href="/" className="header-logo">Ke Li</a>
@@ -14,6 +31,7 @@ function Header () {
         </section>
 
     </section>
+    </div>
   );
 }
 
