f9e6eecbcd840b4e10d0b1723227a9e7070a02a5
Damian Li <ke_2021@126.com>
2023-12-16 03:50:53 -0500

    finish about :)

--- frontend-pw/src/component/about/about.js
+++ frontend-pw/src/component/about/about.js
@@ -2,20 +2,13 @@ import React from "react";
 import Astronaut from "./images/astronot.json";
 import Lottie from "lottie-react";
 import aboutPhoto from "./images/aboutphoto.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPassport, faDumbbell, faHeadphonesSimple, faBasketball, faPersonSwimming, faCamera } from '@fortawesome/free-solid-svg-icons'
 import "./about.css";
 
 
 function About () {
     // 通过style属性动态设置背景图片
-    const photoStyle = {
-        backgroundImage: `url(${aboutPhoto})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        // 设定图片的高度和宽度
-        width: '100%', // 或者是 '30vw' 根据需要
-        height: '100%' // 或者是 '20vw' 根据需要
-    };
 
     return (
         <div id="about">
@@ -23,31 +16,39 @@ function About () {
                 <h1>About Me</h1>
             </div>
             <div className="about-description">
-                <div className="photo" style={photoStyle}></div>
+                
+                <div className="photo">
+                    <img src={aboutPhoto} alt="aboutphoto" />
+                </div>
                 <div className="text">
                     <h1>What I hope to do</h1>
                     
                     <p className="about-text">I aspire to become a distributed system engineer 
-                    or software development engineer, driven by my passion for creating innovative 
-                    solutions in the tech industry.</p>
+                    / software development engineer, driven by my passion for creating innovative 
+                    solutions in the tech industry.
+                    </p>
+
+                    <h1>What I'm doing</h1>
+
+                    <p className="about-text">
+                    I'm currently pursuing my Master degree at University of Toronto, 
+                    my major study areas are Computing Engineering and Cybersecurity.
+                    I'm seeking 4 or 8 months internships starting from Summer 2024.
+                    </p>
+
+                    <h1>When I'm not coding, you can find me</h1>
+
+                    <p className="about-text">
+                    <FontAwesomeIcon icon={faPassport} /> Travelling | <FontAwesomeIcon icon={faBasketball} /> Basketball | <FontAwesomeIcon icon={faDumbbell} /> Working out | <FontAwesomeIcon icon={faHeadphonesSimple} /> Sportifying |
+
+                    </p>
+                    <p className="about-text">
+                    <FontAwesomeIcon icon={faPersonSwimming} /> Swimming | <FontAwesomeIcon icon={faCamera} /> Photographing | 
+                    </p>
                 </div>
             </div>
             <Lottie animationData={Astronaut} />
         </div>
-        // <div id="about">
-        //     <div className="title">
-        //         <h1>About Me</h1>
-        //     </div>
-        //     <div className="about-description">
-        //             <h1>What I hope to do</h1>
-        //             <p className="about-text">I aspire to become a distributed system engineer 
-        //             or software development engineer, driven by my passion for creating innovative 
-        //             solutions in the tech industry.</p>
-        //     </div>
-            
-            
-            
-        // </div>
     );
 }
 
