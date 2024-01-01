b68c3e6742acbbb987498b24fd3fbb501cbdb412
Damian Li <ke_2021@126.com>
2023-12-31 23:34:48 -0500

    update the selectors

--- frontend-pw/src/component/project/project.js
+++ frontend-pw/src/component/project/project.js
@@ -2,10 +2,15 @@ import React from "react";
 import tree1 from "./tree.json";
 import Lottie from "lottie-react";
 import decorations from './decorations.json';
+import { Select } from 'antd';
+
 import "./project.css";
 
 
 function Project() {
+    const handleChange = (value) => {
+        console.log(`selected ${value}`);
+        };
 
     return(
         <div id="projects">
@@ -19,6 +24,50 @@ function Project() {
             
             <div className="title">
                 <h1>Projects</h1>
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Select
+                defaultValue="Web Application"
+                className="select-width"
+                onChange={handleChange}
+                options={[
+                {
+                    label: 'Team Projects',
+                    options: [
+                    {
+                        label: 'Web Application',
+                        value: 'Web_Application',
+                    },
+                    {
+                        label: 'Mobile Application',
+                        value: 'Mobile_Application',
+                    },
+                    {
+                        label: 'CyberSecurity',
+                        value: 'CyberSecurity',
+                    },
+                    ],
+                },
+                {
+                    label: 'Individual Projects',
+                    options: [
+                    {
+                        label: 'Linux System',
+                        value: 'Linux_System',
+                    },
+                    {
+                        label: 'Network Protocols',
+                        value: 'Network_Protocols',
+                    },
+                    {
+                        label: 'Distributed System',
+                        value: 'Distributed_System',
+                    },
+                    ],
+                },
+                ]}
+            />
             </div>
             <Lottie animationData={tree1} />
         </div>
