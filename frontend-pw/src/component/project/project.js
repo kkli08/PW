04c56fb1174ed01dcb6f611de0aa5dee66b787bc
Damian Li <ke_2021@126.com>
2024-01-14 23:56:12 -0500

    update project

--- frontend-pw/src/component/project/project.js
+++ frontend-pw/src/component/project/project.js
@@ -1,15 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import tree1 from "./tree.json";
 import Lottie from "lottie-react";
 import decorations from './decorations.json';
 import { Select } from 'antd';
+import { Card } from "../card/card";
 
 import "./project.css";
+import habotImage from './project_cover/habot.png';
+import veImage from './project_cover/ve.png';
+
+// Define the card data
+const webApplicationCards = [
+    { 
+        title: 'Web Application - Vocabulary Explorer', 
+        description: 'An online visualization dictionary that allows users to expand or collapse different semantic classes through the form of a tree in order to see which word(s) fall under that category, and the relationships between categories.\n\nTool Use: React, D3.js, JavaScript, AWS, Docker, Github, MKDoc, Figma', 
+        imgSrc: veImage, // 图片的路径
+        imgAlt: 'Image',// 图片的替代文本
+        link: 'https://www.youtube.com/watch?v=0xyK7al-No4',
+    },
+    { 
+        title: 'Web Application - ', 
+        description: '\n\nTool Use: ', 
+        imgSrc: habotImage, // 图片的路径
+        imgAlt: 'Image',// 图片的替代文本
+        link: '',
+    },
+    { 
+        title: 'Web Application - ', 
+        description: '\n\nTool Use: ', 
+        imgSrc: habotImage, // 图片的路径
+        imgAlt: 'Image',// 图片的替代文本
+        link: '',
+    },
+    { 
+        title: 'Web Application - ', 
+        description: '\n\nTool Use: ', 
+        imgSrc: habotImage, // 图片的路径
+        imgAlt: 'Image',// 图片的替代文本
+        link: '',
+    },
+];
+
+const mobileApplicationCards = [
+    { 
+        title: 'Android Application - Habot', 
+        description: 'An android application that aims to help people record their daily habits, including adding, deleting, changing habits, and presenting the habits of the day separately.\n\nTool Use: Java, Android Studio, Github, Firebase, Figma', 
+        imgSrc: habotImage, // 图片的路径
+        imgAlt: 'Mobile App 1 Image',// 图片的替代文本
+        link: 'https://github.com/CMPUT301F21T24/Habot/wiki/Part4-StoryBoard',
+    },
+    // ... more cards
+];
 
 
 function Project() {
+    const [cards, setCards] = useState(webApplicationCards);
+
     const handleChange = (value) => {
         console.log(`selected ${value}`);
+        switch (value) {
+            case 'Web_Application':
+                setCards(webApplicationCards);
+                break;
+            case 'Mobile_Application':
+                setCards(mobileApplicationCards);
+                break;
+            // ... handle other cases
+            default:
+                setCards([]);
+        }
         };
 
     return(
@@ -69,6 +128,20 @@ function Project() {
                 ]}
             />
             </div>
+
+            <div className="boxcol">
+                {cards.map((card, index) => (
+                    <Card 
+                        key={index} 
+                        title={card.title} 
+                        description={card.description} 
+                        imgSrc={card.imgSrc}
+                        imgAlt={card.imgAlt}
+                        link={card.link}
+                    />
+                ))}
+            </div>
+
             <Lottie animationData={tree1} />
         </div>
     );
