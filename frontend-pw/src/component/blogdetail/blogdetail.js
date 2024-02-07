cf2802a4189dbf46ac69adf044bad5b8748eac4c
Damian Li <ke_2021@126.com>
2024-02-07 11:04:12 -0500

    update hmac blog

--- frontend-pw/src/component/blogdetail/blogdetail.js
+++ frontend-pw/src/component/blogdetail/blogdetail.js
@@ -8,28 +8,36 @@ import bufferoverflowImage from '../blog/blog_cover/buffer_overflow.png';
 import securityauditImage from '../blog/blog_cover/security_audit.png';
 import doublefreeImage from '../blog/blog_cover/double_free.jpg';
 import formatstringImage from '../blog/blog_cover/format_string.png';
+import hmacImage from '../blog/blog_cover/hmac.png'; 
 
 const cybersecurityCards = [
     { 
+        title: 'HMAC Authentication', 
+        description: 'HMAC is a type of message authentication code (MAC) involving a cryptographic hash function and a secret cryptographic key. It is used to verify the data integrity and the authentication of a message.\n\n[Integrity][Authenticity]', 
+        imgSrc: hmacImage, // 图片的路径
+        imgAlt: 'hmac Image',// 图片的替代文本
+        link: 'https://github.com/kkli08/HMAC/wiki#hmac',
+    },
+    { 
         title: 'Buffer Overflow', 
-        description: 'Buffer overflow is a vulnerability in low level programming languages such as C and C++. It is caused by writing data beyond the allocated memory. This can lead to a crash of the program or even worse, the attacker can execute arbitrary code.', 
+        description: 'Buffer overflow is a vulnerability in low level programming languages such as C and C++. It is caused by writing data beyond the allocated memory. This can lead to a crash of the program or even worse, the attacker can execute arbitrary code.\n\n[Binary Exploitation][Stack]', 
         imgSrc: bufferoverflowImage, // 图片的路径
         imgAlt: 'buffer overflow Image',// 图片的替代文本
         link: 'https://github.com/kkli08/Buffer-Overflow/wiki#welcome-to-my-buffer-overflow-blog',
     },
     { 
-        title: 'Format String Overflow', 
-        description: 'Format string vulnerability is a type of software vulnerability that can be used in security exploits. This can lead to a exploitation, the attacker can execute arbitrary code by overwriting the return address.', 
-        imgSrc: formatstringImage, // 图片的路径
-        imgAlt: 'format string Image',// 图片的替代文本
-        link: 'https://github.com/kkli08/format-string/wiki#format-string',
-    },
-    { 
         title: 'Double Free Exploitation', 
-        description: 'Double free is a memory corruption bug that occurs when free() is called on the same allocated memory address more than once. This can lead to a exploitation, the attacker can execute arbitrary code by assigning the shellcode address(in the heap) to the return address($rip).', 
+        description: 'Double free is a memory corruption bug that occurs when free() is called on the same allocated memory address more than once. This can lead to a exploitation, the attacker can execute arbitrary code by assigning the shellcode address(in the heap) to the return address($rip).\n\n[Binary Exploitation][Heap]', 
         imgSrc: doublefreeImage, // 图片的路径
         imgAlt: 'double free Image',// 图片的替代文本
         link: 'https://github.com/kkli08/format-string/wiki#double-free',
+    },
+    { 
+        title: 'Format String Overflow', 
+        description: 'Format string vulnerability is a type of software vulnerability that can be used in security exploits. This can lead to a exploitation, the attacker can execute arbitrary code by overwriting the return address.\n\n[Binary Exploitation][Stack]', 
+        imgSrc: formatstringImage, // 图片的路径
+        imgAlt: 'format string Image',// 图片的替代文本
+        link: 'https://github.com/kkli08/format-string/wiki#format-string',
     },
     { 
         title: 'Security Audit', 
