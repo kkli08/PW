9808fa10114ee8b2adcc899acbd271607b52b379
Damian Li <ke_2021@126.com>
2024-02-14 12:54:11 -0500

    add google analytics

--- frontend-pw/src/App.js
+++ frontend-pw/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Home from './pages/home/home';
 import {
@@ -6,13 +6,30 @@ import {
   Routes,
   Route,
   Link,
+  useLocation
 } from "react-router-dom";
 import Blogpage from './pages/blogpage/blogpage';
 import Gallerypage from './pages/gallerypage/gallerypage';
+import ReactGA from 'react-ga';
+
+function GAListener() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize('G-M4B3ZPELJN');
+  }, []);
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
+  return null; // This component does not render anything
+}
 
 function App() {
   return (
     <Router>
+      <GAListener /> {/* This component will handle the GA tracking */}
       <Routes>
         <Route path="/" Component={Home}/>
         <Route path="/blog" Component={Blogpage}/>
